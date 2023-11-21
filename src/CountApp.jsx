b7a62cc31dbf8758b9f070831be8d11a401aaff0
joselib <54868005+joselib@users.noreply.github.com>
2023-11-21 01:22:11 +0000

    hooks part 1

--- src/CountApp.jsx
+++ src/CountApp.jsx
@@ -1,10 +1,26 @@
+import {useState} from 'react';
 import PropTypes from 'prop-types';
 
-export const CountApp = ({value}) => {
+export const CountApp = ({ value }) => {
+
+    const [ counter, setCounter] = useState( value )
+
+    const handleAdd = (event) => {
+        
+        //console.log(event)
+        setCounter( counter + 1)
+        //setCounter( (c) => c + 1)
+
+    }
+    
+
     return (
         <>
             <h1>Count App</h1>
-            <h2>{ value }</h2>
+            <h2> { counter } </h2>
+            <button onClick={ handleAdd}>
+                +1
+            </button>
         </>
     );
 };
